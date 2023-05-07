55131690173f85a7b06e38e869b3fde7bf830e7e
fadeto404 <rasmus.skjelsager@gmail.com>
2023-05-07 19:15:29 +0200

    Fixed quaternion to rotatation matrix conversion

--- scripts/util/q2R.m
+++ scripts/util/q2R.m
@@ -1,3 +1,8 @@
 function R_q = q2R(q)
-    R_q = (q(4)^2 + q(1:3)'*q(1:3))*eye(3) - 2*q(4)*cross_prod_mat(q) + 2*q(1:3)*q(1:3)';
+% q2R: Compute equivalent rotation matrix from quaternion.
+% Inputs:
+%   q:      4 x 1 vector/quaternion [x; y; z; w] (or [q1; q2; q3; q4])
+% Outputs:
+%   R_q:    3 x 3 rotation matrix equivalent to q
+    R_q = (q(4)^2 - transpose(q(1:3))*q(1:3))*eye(3) - 2*q(4)*cross_prod_mat(q(1:3)) + 2*q(1:3)*transpose(q(1:3));
 end
